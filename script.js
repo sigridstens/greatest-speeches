d97ecb19305623db5a1f7a014c18bf29ae4c253e
Sigrid Stensvold <sigridstens@gmail.com>
2019-02-15 08:19:31 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('BtnDonate').addEventListener('click', function(){
+  //Code in here executes when the user clicks the "Donate" button.
+});
+
+document.getElementById('BtnChurchill').addEventListener('click', function(){
+  //Code in here executes when the user clicks the "Churchill" button.
+});
+
+document.getElementById('BtnGhandi').addEventListener('click', function(){
+  //Code in here executes when the user clicks the "Ghandi" button.
+});
+
+document.getElementById('BtnDemosthenes').addEventListener('click', function(){
+  //Code in here executes when the user clicks the "Demosthenes" button.
+});
